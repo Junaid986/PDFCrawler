c33e901423fea65b103e1546d05533342f28b15c
Junaid986 <junaidramis@gmail.com>
2017-11-03 16:11:27 +0800

    Bug fixes

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import threading;
 from pdfSpider import PDFSpider;
 import sys;
 
-if __name__ == "__main__":
+def main():
     params = sys.argv
-    if(len(params) > 2):
-        spider = PDFSpider(params[1], params[2])
+    if (len(params) > 2):
+        PDFSpider(params[1], params[2])
     else:
         print("Enter Module Name and Folder Directory")
+        moduleName = input("Enter Module Name: ")
+        path = input("Enter path of Pdfs: ")
+        PDFSpider(moduleName, path)
+
+if __name__ == "__main__":
+    main()
