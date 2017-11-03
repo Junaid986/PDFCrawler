46d351c0a0a30759c0a83b77f022ffb11fb7bd0c
Junaid986 <junaidramis@gmail.com>
2017-11-03 17:16:19 +0800

    Added blank divider page functionality

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import sys;
 def main():
     params = sys.argv
     if (len(params) > 2):
-        PDFSpider(params[1], params[2])
+         PDFSpider(*params[1:])
     else:
         print("Enter Module Name and Folder Directory")
         moduleName = input("Enter Module Name: ")
         path = input("Enter path of Pdfs: ")
-        PDFSpider(moduleName, path)
+        ifBlankDivided = input("Enter if pdfs is empty page divided(0/1): ")
+        PDFSpider(moduleName, path, ifBlankDivided)
 
 if __name__ == "__main__":
     main()
--- pdfSpider.py
+++ pdfSpider.py
@@ -6,11 +6,19 @@ class PDFSpider:
 
     module_name = ""
     base_path = ""
+    is_blank_page_divided = False
+    blank_path = "blank.pdf"
+    emptyPdf = None
 
-    def __init__(self, module, base_path):
+    def __init__(self, module, base_path, is_blank_page_divided = '0'):
         sys.setrecursionlimit(100000)
         PDFSpider.module_name = module
         PDFSpider.base_path = base_path
+        if is_blank_page_divided:
+            if is_blank_page_divided == '1':
+                PDFSpider.is_blank_page_divided = True
+            elif is_blank_page_divided == '0':
+                PDFSpider.is_blank_page_divided = False
         PDFSpider.boot()
 
     @staticmethod
@@ -18,10 +26,15 @@ class PDFSpider:
         fh.create_project_dir(PDFSpider.module_name)
         merger = PdfFileMerger()
         files = fh.get_pdf_list_from_directory(PDFSpider.base_path)
+        if(PDFSpider.is_blank_page_divided):
+            # with open(PDFSpider.blank_path, "rb") as f:
+            PDFSpider.emptyPdf = PdfFileReader(open(PDFSpider.blank_path, "rb"))
         for f in files:
             print(f)
             file = open(f, "rb")
             merger.append(PdfFileReader(file))
+            if (PDFSpider.is_blank_page_divided):
+                merger.append(PDFSpider.emptyPdf)
             file.close()
         with open(PDFSpider.module_name + "/" + PDFSpider.module_name + ".pdf", 'wb') as modDir:
             merger.write(modDir)
